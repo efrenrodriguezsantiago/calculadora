605a85e4d73664bd3d681f0e7b7ebdfe1a36e420
Efren Rodriguez <1717110246@utectulancingo.edu.mx>
2021-03-02 15:11:02 -0600

    imprime en pantalla con metodo constructor

--- _2proyecto.js
+++ _2proyecto.js
@@ -1,18 +1,23 @@
 document.write("-----------Bienvenido---------");
 class Book{
-    titulo = '1985';
-    autor = 'george urwell';
-    precio = 500;
+    //metodo constructor
+    constructor(titulo,autor,precio){
+        this._titulo = titulo;
+        this._autor = autor;
+        this._precio = precio;
+    }
+
 }
 //instancia de objetos
-const book1 = new Book();
-const book2 = new Book();
+const book1 = new Book('1985','george urwell',300);
+const book2 = new Book('frankeisten','M. S',200);
 
-//imprime en pantall
-document.write(book1.titulo);
-document.write(book1.autor);
-document.write(book1.precio);
+//imprime en pantalla
+document.write(book1._titulo);
+document.write(book1._autor);
+document.write(book1._precio);
 
-document.write(book2.titulo);
-document.write(book2.autor);
-document.write(book2.precio);
+//imprime en pantalla
+document.write(book2._titulo);
+document.write(book2._autor);
+document.write(book2._precio);
