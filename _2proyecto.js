6cfbcdd71efde59420310eacfa5e799cad6be72d
Efren Rodriguez <1717110246@utectulancingo.edu.mx>
2021-03-02 15:05:06 -0600

    imprime en pantalla en consola con book y clases

--- _2proyecto.js
+++ _2proyecto.js
@@ -1,3 +1,18 @@
 document.write("-----------Bienvenido---------");
-document.write("hola mundo");
-document.write("proyecto con poo");
+class Book{
+    titulo = '1985';
+    autor = 'george urwell';
+    precio = 500;
+}
+//instancia de objetos
+const book1 = new Book();
+const book2 = new Book();
+
+//imprime en pantall
+document.write(book1.titulo);
+document.write(book1.autor);
+document.write(book1.precio);
+
+document.write(book2.titulo);
+document.write(book2.autor);
+document.write(book2.precio);
